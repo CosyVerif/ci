a1c2d3fff6aedde00b9411ecfdd128540464f1e5
Alban Linard <alban@linard.fr>
2015-12-11 17:12:34 +0100

    Update example to show file copy.

--- examples/ci.conf.lua
+++ examples/ci.conf.lua
@@ -5,17 +5,22 @@ return function ()
   local systems = {
     debian = {
       dev  = {
-        identifier = "Ubuntu 12.04",
-        user       = "alinard",
+        identifier = "debian-stable-amd64",
+        user       = "cosyverif",
       },
     },
   }
-  for name, system in pairs (systems) do
-    print ("Building on " .. name)
-    local build_client = VM (system.dev)
-    build_client [[ git clone "https://github.com/CosyVerif/library.git" ]]
-    local client_output = build_client [[ cd library && ./bin/build/client ]]
-    print (client_output)
-  end
-
+  local build_client = VM (systems.debian.dev)
+  build_client [[ sudo apt-get update  --yes     ]]
+  build_client [[ sudo apt-get install --yes git ]]
+  build_client [[ git clone "https://github.com/CosyVerif/library.git" ]]
+  build_client ([[ cd library && git checkout {{{branch}}} ]] % {
+    branch = "issue-158",
+  })
+  local result = build_client [[ cd library && ./bin/build-client ]]
+  local path   = result:match "Packaging in (.*%.tar%.gz)"
+  local run_client = VM (systems.debian.dev)
+  run_client ["cosy-client.tar.gz"] = build_client [path]
+  run_client [[ tar xvf cosy-client.tar.gz ]]
+  run_client [[ ls && ls cosy && ls cosy/client ]]
 end
