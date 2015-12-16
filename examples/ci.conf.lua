457601e292573ed50c38be41e26486d86c82f949
Alban Linard <alban@linard.fr>
2015-12-16 13:14:22 +0100

    Working example without port redirections.

--- examples/ci.conf.lua
+++ examples/ci.conf.lua
@@ -1,26 +1,73 @@
 local VM = require "ci.vm"
 
-return function ()
-
-  local systems = {
-    debian = {
-      dev  = {
-        identifier = "debian-stable-amd64",
-        user       = "cosyverif",
+local systems = {
+  debian = {
+    stable = {
+      amd64 = {
+        dev  = {
+          identifier = "debian-stable-amd64",
+          user       = "cosyverif",
+        },
+      },
+      i386 = {
+        dev  = {
+          identifier = "debian-stable-i386",
+          user       = "cosyverif",
+        },
       },
     },
-  }
-  local build_client = VM (systems.debian.dev)
-  build_client [[ sudo apt-get update  --yes     ]]
-  build_client [[ sudo apt-get install --yes git ]]
-  build_client [[ git clone "https://github.com/CosyVerif/library.git" ]]
-  build_client ([[ cd library && git checkout {{{branch}}} ]] % {
-    branch = "issue-158",
-  })
-  local result = build_client [[ cd library && ./bin/build-client ]]
-  local path   = result:match "Packaging in (.*%.tar%.gz)"
-  local run_client = VM (systems.debian.dev)
-  run_client ["cosy-client.tar.gz"] = build_client [path]
-  run_client [[ tar xvf cosy-client.tar.gz ]]
-  run_client [[ ls && ls cosy && ls cosy/client ]]
+  },
+}
+
+return function (t)
+  local copev = t.copev
+
+  for _, system in ipairs {
+    systems.debian.stable.amd64,
+  } do
+    local vmm = VM.new {
+      copev = copev,
+    }
+    copev.addthread (function ()
+      local main = copev.running ()
+      local client_archive
+      copev.addthread (function ()
+        local build_client = vmm (system.dev)
+        build_client [[ sudo apt-get update  --yes ]]
+        build_client [[ sudo apt-get install --yes git ]]
+        build_client [[ git clone "https://github.com/CosyVerif/library.git" ]]
+        build_client ([[ cd library && git checkout {{{branch}}} ]] % {
+          branch = "issue-158",
+        })
+        local result = build_client [[ cd library && ./bin/build-client --in-ci]]
+        local path   = result:match "(%S*cosy%-client%-.-%.tar%.gz)"
+        client_archive = build_client [path]
+        copev.wakeup (main)
+      end)
+      local server = nil
+      copev.addthread (function ()
+        local build_server = vmm (system.dev)
+        build_server [[ sudo apt-get update  --yes ]]
+        build_server [[ sudo apt-get install --yes git ]]
+        build_server [[ git clone "https://github.com/CosyVerif/library.git" ]]
+        build_server ([[ cd library && git checkout {{{branch}}} ]] % {
+          branch = "issue-158",
+        })
+        build_server [[ cd library && ./bin/build-server --prefix=/home/cosyverif --in-ci]]
+        server = true
+        copev.wakeup (main)
+      end)
+
+      repeat
+        copev.sleep (-math.huge)
+      until client_archive and server
+
+      local run_client = vmm (system.dev)
+      run_client ["cosy-client.tar.gz"] = client_archive
+      run_client [[ tar xvf cosy-client.tar.gz ]]
+      run_client [[ ./usr/bin/cosy-version ]]
+      run_client [[ ./usr/bin/cosy ]]
+    end)
+  end
+
 end
